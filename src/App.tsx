7b49a4d5cb3511b0737d8ee8f6d9a2c0070dcb39
Memnoc <smartdroidesign@gmail.com>
2024-12-18 05:47:53 +0000

    shadcn init

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello Tailwind</h1>
+      <Button>Shad init </Button>
     </>
   );
 }
