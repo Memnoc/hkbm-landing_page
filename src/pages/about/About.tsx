facd34490ed0bf8f9142d01b916fcd43c841e4ad
Memnoc <smartdroidesign@gmail.com>
2025-01-18 13:28:53 +0000

    initial layout for About sectoin

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -3,9 +3,14 @@ import BackButton from "@/components/custom/BackButton";
 export default function About() {
   return (
     <div className="pt-16 animate-fade-in">
+      <div className="title-container flex flex-col mt-32 pb-16 text-center">
+        <h1 className="text-3xl font-bold text-purple-700 mb-6">About</h1>
+        <p className="text-gray-600">
+          Follow these steps to complete your purchase
+        </p>
+      </div>
       <div className="our-story-container animate-fade-in transition-all duration-500 ease-in-out mx-auto px-8">
         <BackButton />
-        <h1 className="text-3xl font-bold text-purple-700 mb-6">About</h1>
       </div>
     </div>
   );
